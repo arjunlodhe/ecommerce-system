4a3eb3926de77be751d8ff0b96cb36082ad504d1
Arjun Lodhe <arjunlodhe@gmail.com>
2025-11-24 20:29:35 +0530

    Initial commit: Adding product microservice

---
+++ product-service/src/test/java/com/pc/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pc.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
